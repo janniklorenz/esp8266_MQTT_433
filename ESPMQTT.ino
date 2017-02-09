ca2a62e7d627a6e04d4aee8a6a410eafdefedb8c
Jannik Lorenz <dev@janniklorenz.de>
2017-02-09 02:04:19 +0100

    clean up

--- ESPMQTT.ino
+++ ESPMQTT.ino
@@ -14,80 +14,97 @@ const int RF_OSC = 200;
 WiFiClient espClient;
 PubSubClient client(espClient);
 
-void callback(char* topic, byte* payload, unsigned int length) {
-  Serial.print("Message arrived ");
-  Serial.print(topic);
 
-  // put the payload in a null terminated string
-  char *message = (char*)malloc(sizeof(char)*length+1);
-  strncpy(message, (char*)payload,length);
-  message[length] = '\0';
+// called when we receive a message in a room we have subscribed
+// topic      the room the message came from
+// payload    raw payload data
+// length     length of the payload
+void mqttReceivedCallback(char* topic, byte* payload, unsigned int length) {
+	Serial.print("Message arrived ");
+	Serial.print(topic);
 
-  // check if the topic is mqtt_set
-  if (strcmp(topic, mqtt_set) == 0) {
-    int value = atoi(message);
-    mySwitch.send(value, 24);
-    delay(100);
-    client.publish(mqtt_didSet, message);
-  }
-  
+	// put the payload in a null terminated string
+	char *message = (char*)malloc(sizeof(char)*length+1);
+	strncpy(message, (char*)payload,length);
+	message[length] = '\0';
+
+	// check if the topic is mqtt_set
+	if (strcmp(topic, mqtt_set) == 0) {
+		int value = atoi(message);
+		mySwitch.send(value, 24);
+		delay(100);
+		client.publish(mqtt_didSet, message);
+	}
+
 }
 
 
-void reconnect() {
-  // Loop until we're reconnected
-  while (!client.connected()) {
-    Serial.print("Attempting MQTT connection...");
-    // Attempt to connect
-    if (client.connect("ESP8266 Client")) {
-      Serial.println("connected");
-      
-      // ... and subscribe to topic
-      client.subscribe(mqtt_set);
-    }
-    else {
-      Serial.print("failed, rc=");
-      Serial.print(client.state());
-      Serial.println(" try again in 5 seconds");
-      Serial.print("IP address: ");
-      Serial.println(WiFi.localIP());
 
-      // Wait 5 seconds before retrying
-      delay(5000);
-    }
-  }
+// (Re)Connect to MQTT Server
+void connectMQTT() {
+	// Loop until we're reconnected
+	while (!client.connected()) {
+		Serial.print("Attempting MQTT connection...");
+		// Attempt to connect
+		if (client.connect("ESP8266 Client")) {
+			Serial.println("connected");
+
+			// ... and subscribe to topic
+			client.subscribe(mqtt_set);
+		}
+		else {
+			Serial.print("failed, rc=");
+			Serial.print(client.state());
+			Serial.println(" try again in 5 seconds");
+			Serial.print("IP address: ");
+			Serial.println(WiFi.localIP());
+
+			// Wait 5 seconds before retrying
+			delay(5000);
+		}
+	}
 }
 
-void setup()
-{
-  //pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(0, OUTPUT);
-  mySwitch.enableTransmit(0);
-
-  Serial.begin(9600);
-  WiFi.begin(ssid, password);
-
-  // Wait for connection
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print("-");
-  }
-  Serial.println("");
-  Serial.print("Connected to ");
-  Serial.println(ssid);
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
-
-  client.setServer(mqtt_server, 1883);
-  client.setCallback(callback);
-
-  //pinMode(ledPin, OUTPUT);
+// (Re)Connect the WIFI
+void connectWIFI() {
+	WiFi.begin(ssid, password);
+	while (WiFi.status() != WL_CONNECTED) {
+		delay(500);
+		Serial.print(".");
+	}
+	Serial.println("");
+	Serial.print("Connected to ");
+	Serial.println(ssid);
+	Serial.print("IP address: ");
+	Serial.println(WiFi.localIP());
 }
 
-void loop()
-{
-  if (!client.connected()) {
-    reconnect();
-  }
-  client.loop();
+
+
+void setup() {
+	// Set up pin rfPin
+	pinMode(rfPin, OUTPUT);
+	mySwitch.enableTransmit(rfPin);
+
+	// Debug serial
+	Serial.begin(9600);
+
+	// Init MQTT client
+	client.setServer(mqtt_server, 1883);
+	client.setCallback(mqttRecevedCallback);
+}
+
+void loop() {
+	// If we have no WIFI connecton, trigger connectWIFI
+	if (WiFi.status() != WL_CONNECTED) {
+		connectWIFI();
+	}
+
+	// If we have no connecton to the MQTT Server, trigger connectMQTT
+	if (client.connected() == false) {
+		connectMQTT();
+	}
+
+	// Otherwise run MQTT client loop
+	client.loop();
 }
--- config.example.h
+++ config.example.h
@@ -1,8 +1,22 @@
 // MQTT Rooms
+
+// Room to listen for updates
 const char *mqtt_set = "";
+
+// Room to perform receive action
 const char *mqtt_didSet = "";
+
+// MQTT Server URL
+const char *mqtt_server = "";
+
+
 
 // WIFI
 const char *ssid = "";
 const char *password = "";
-const char *mqtt_server = "";
+
+
+
+// RF
+// Pin where the 433 sender is connected to
+int rfPin = 0;
